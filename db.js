6dd21815befbeac637d496ff3da7e4f11058fcb1
mego <megocapps@gmail.com>
2023-05-25 11:47:17 -0400

    DB config

--- db.js
+++ db.js
@@ -1,2 +1,15 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'megan',
+  host: 'localhost',
+  database: 'biztime',
+  password: 'mego',
+  port: 5432, // Default PostgreSQL port
+});
+
+client.connect();
+
+module.exports = client;
