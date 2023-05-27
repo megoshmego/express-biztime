3ef8facdb9f30c61e94c7aedb1830c4a79f5437e
mego <megocapps@gmail.com>
2023-05-27 15:05:12 -0400

    middleware - parse JSON

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-/** BizTime express application. */
-
-
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const ExpressError = require("./expressError")
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices');
 
 app.use(express.json());
 
+// Mount the companies routes
+app.use('/companies', companiesRoutes);
 
-/** 404 handler */
+// Mount the invoices routes
+app.use('/invoices', invoicesRoutes);
 
+// 404 handler
 app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-/** general error handler */
-
-app.use((err, req, res, next) => {
+// Error handler
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-
   return res.json({
-    error: err,
-    message: err.message
+    error: err.message || 'Internal Server Error',
   });
 });
-
 
 module.exports = app;
