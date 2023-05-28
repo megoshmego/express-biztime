97bf9e6421f4dc48a02ef50cbd552f073e908e3a
mego <megocapps@gmail.com>
2023-05-28 13:25:08 -0400

    password

--- db.js
+++ db.js
@@ -1,16 +1,12 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: 'postgresql://megan@localhost/biztime',
+  connectionString: 'postgresql://megan:mego@localhost/biztime',
 });
-
 
 client.connect();
 
-
-
 module.exports = client;
-
 
 
 
